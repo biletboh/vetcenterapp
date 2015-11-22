d1d2001477614532925720da56633f6815cd043f
biletboh <biletskyboh@gmail.com>
2015-11-22 23:27:02 +0000

    base html agancy

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, url_for, redirect, request 
 from myapp import app 
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
